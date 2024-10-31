11cb8d2459eadee18f82f531c8d6f4e1db1ed8b4
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2024-10-31 23:44:28 +0000

    Fix nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,4 @@
----@diagnostic disable: different-requires
 require('config.settings')    -- Basic vim settings.
 require('config.keymaps')     -- Setup keys + shortcuts.
 require('config.lazy')        -- Setup lazy nvim.
-require('config.plugins')     -- Setup plugins.
 require('config.colorscheme') -- Sets default colorscheme.
