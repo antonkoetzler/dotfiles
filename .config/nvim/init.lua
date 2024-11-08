e99c8dfe6c32ac9769e78b1a4b13301c14969f0a
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2024-11-08 20:52:21 -0300

    Submodule time

--- .config/nvim/init.lua
+++
@@ -1,4 +0,0 @@
-require('config.settings')    -- Basic vim settings.
-require('config.keymaps')     -- Setup keys + shortcuts.
-require('config.lazy')        -- Setup lazy nvim.
-require('config.colorscheme') -- Sets default colorscheme.
