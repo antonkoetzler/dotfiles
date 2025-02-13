c9508bbf0b76cd73358a88a316866f3b1d882086
Anton Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2025-02-12 21:54:06 -0300

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -11,79 +11,43 @@ yay -Syu
 
 # Principal dependencies.
 yay -S \
-  # Developer tools like CMake.
   base-devel \
-
-  # Display communication protocol.
   wayland \
-
-  # Login Manager
   qt5-quickcontrols \
   qt6 \
   sddm \
-
-  # Hypr suite: Tilting window, wallpaper, & lock managers.
   hyprland \
   hyprpaper \
   hyprlock \
   hypridle \
-
-  # Statusbar
   waybar \
-
-  # Application launcher/searcher.
   wofi \
-
-  # Default font.
   otf-font-awesome \
   ttf-hack-nerd \
-
-  # Terminal.
   kitty \
-
-  # Browser.
   firefox \
-
-  # File explorer.
   dolphin \
-
-  # Graphics packages (nvidia users hush plz thx).
   xf86-video-amdgpu \
   mesa \
   mesa-utils \
   vulkan-radeon \
   opencl-amd \
-
-  # Screenshots
   grim \
   slurp \
   wf-recorder \
-
-  # Auto-mounting USBs.
   ntfs-3g \
   udiskie \
-
-  # Network manager.
   network-manager \
-
-  # Laptop brightness manager.
   brightnessctl \
-
-  # Disk viewing utility.
   gnome-disk-utility \
-
-  # Git tab completion.
   git-completion \
-
-  # For live grep with telescope in Neovim.
   ripgrep \
-
-  # Technologies that https://github.com/antonkoetzler/nvim-config needs.
   go \
   npm
 
 # xdg-desktop-portal-hyprland. This is used for compatbility of applications in hyprland for a smoother experience.
 yay -S \
+  cmake \
   gbm \
   hyprland-protocols \
   hyprlang \
@@ -108,6 +72,7 @@ sudo systemctl enable NetworkManager
 read -p "Install https://github.com/antonkoetzler/nvim-config? THIS WILL DELETE YOUR NEOVIM CONFIGURATION. (Y/n): " -r answer
 answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]') # Allow answer to be validated case-insensitively.
 if [[ "$answer" == "y" ]]; then
-  rm -rf nvim
+  yay -S neovim
+  rm -rf $USER/.config/nvim
   git clone git@github.com:antonkoetzler/nvim-config .config/nvim
 fi
